ec1361b38a76e3c700fbd4d2f34a325a0f42034f
godharrylin <godharrylin@gmail.com>
2026-07-02 19:57:39 +0800

    Remove useless package

--- src/gym-system.Api/Program.cs
+++ src/gym-system.Api/Program.cs
@@ -102,17 +102,9 @@ builder.Services.AddScoped<CancelScheduleSessionHandler>();
 //builder.Services.AddScoped<RegisterMemberHandler>();
 //builder.Services.AddInfrastructureInMemory();
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
 app.UseCors("Frontend");
